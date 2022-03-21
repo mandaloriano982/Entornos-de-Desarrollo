aa595daf1e705ecfc76923c57749971478ae0ef0
garci_s5a <garci_s5a@PowerPC>
2022-03-21 18:35:49 +0100

    Mi primer repo de GITHUB

---
+++ ud4tarea3/src/ud4tarea3/ud4tarea3.java
@@ -0,0 +1,10 @@
+package ud4tarea3;
+
+public class ud4tarea3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+	}
+
+}
